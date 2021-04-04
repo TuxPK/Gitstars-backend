5438362e80f738d8ecb98656c6aa1960bbe34dc3
TuxPK <marcosxr8@gmail.com>
2021-04-04 17:14:10 -0300

    Token controller removed

--- src/routes/githubAuthenticate.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import getToken from '../app/controllers/github/getToken';
-
-const router = Router();
-
-router.post('/token', getToken);
-
-export default router;
