9f7d73203d20e439cb048b4d3be0d17a7d7a9539
TuxPK <marcosxr8@gmail.com>
2021-04-05 13:23:40 -0300

    Handling errors on get and remove tags controllers.
    Handling errors with try and catch to return response correctly
    to request.

--- src/app/controllers/tag/remove.js
+++ src/app/controllers/tag/remove.js
@@ -4,14 +4,24 @@ export default async (req, res) => {
   const { uuid } = req.params;
 
   try {
+    const response = await deleteUser(uuid);
+
+    return res.jsons(response);
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
+
+async function deleteUser(uuid) {
+  try {
     await Tag.destroy({
       where: {
         uuid,
       },
     });
 
-    return res.json({ message: 'Tag successfully deleted.' });
+    return { message: 'Tag successfully deleted.' };
   } catch (error) {
     throw new Error('Failed with an error in database request.');
   }
-};
+}
