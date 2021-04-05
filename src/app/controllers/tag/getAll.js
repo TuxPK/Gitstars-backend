2c5233a6c48e90103f16943a7450af6dcbc0b2d3
TuxPK <marcosxr8@gmail.com>
2021-04-05 13:23:40 -0300

    Handling errors on get and remove tags controllers.
    Handling errors with try and catch to return response correctly
    to request.

--- src/app/controllers/tag/getAll.js
+++ src/app/controllers/tag/getAll.js
@@ -4,14 +4,24 @@ export default async (req, res) => {
   const { user_id } = req;
 
   try {
+    const tags = await getAll(user_id);
+
+    return res.json(tags);
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
+
+async function getAll(user_id) {
+  try {
     const tags = await Tag.findAll({
       where: {
         user_id,
       },
     });
 
-    return res.json(tags);
+    return tags;
   } catch (error) {
     throw new Error('Failed with an error in database request.');
   }
-};
+}
