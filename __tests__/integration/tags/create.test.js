dc9a678cb28cbefd972067a2a7bafffb6c45b20f
TuxPK <marcosxr8@gmail.com>
2021-04-11 21:50:34 -0300

    Create tag test handling duplicated tags error
    Handling duplicated tags error on creating tag tests

--- __tests__/integration/tags/create.test.js
+++ __tests__/integration/tags/create.test.js
@@ -6,6 +6,8 @@ import auth from '../../util/auth';
 
 const { api_token, user_id, repository_id } = auth();
 
+const duplicatedError = 'This tag already exists.';
+
 describe('createTags', () => {
   beforeAll(async () => {
     await truncate();
@@ -24,6 +26,18 @@ describe('createTags', () => {
     tag.uuid = response.body.uuid;
 
     expect(response.body).toEqual(tag);
+  });
+
+  it('Should not be able to register duplicated tag', async () => {
+    const tag = {
+      name: 'Spring',
+      repository_id,
+    };
+
+    await createTags(tag);
+    const response = await createTags(tag);
+
+    expect([response.status, response.body.error]).toEqual([400, duplicatedError]);
   });
 });
 
